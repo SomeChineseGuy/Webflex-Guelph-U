94631b91fd88f979cac7e927b36c565763e23273
Alvin Ng <alvin.cl.ng@gmail.com>
2023-05-02 19:08:55 -0700

    Added say hello

---
+++ M1W3 Testing with Mocha & Chai/sayhello.js
@@ -0,0 +1,15 @@
+const assert = require('assert')
+
+const sayHello = (name) => {
+  // console.log(`Hello there ${name}`);
+  
+  return `Hello there ${name}`;
+}
+
+const sum = (num1, num2) => {
+  return num1 + num2
+}
+
+console.log('hey how are you?');
+
+module.exports = sayHello
